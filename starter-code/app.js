019646819fae197358e2021c9de7dcbb09f96833
Doro Hassan <dorothea.hassan@gmail.com>
2019-08-31 18:43:30 +0200

    create the website done. still some toDo but ready for monday

---
+++ starter-code/app.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const app = express();
+const hbs = require("hbs");
+const path = require("path");
+const data = require("./data.js");
+
+app.use(express.static(path.join(__dirname, "public")));
+
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "hbs");
+
+hbs.registerPartials(path.join(__dirname, "views/partials"));
+
+
+app.get("/", (req, res) => {
+    res.render("index");
+});
+
+app.get("/about", (req, res) => {
+    res.render("about");
+});
+
+app.get("/photo_gallery", (req, res) => {
+    res.render("photo_gallery", data);
+});
+
+
+
+const listener = app.listen(3050, () => {
+    console.log(`server is up @ http://localhost: ${listener.address().port}`);
+});
+
---
+++ starter-code/data.js
@@ -0,0 +1,55 @@
+
+
+module.exports = [
+    {
+        id: 1,
+        title: "Rottweiler",
+        image: "/images/rottweiler_standing.jpg"
+    },
+    {
+        id: 2,
+        title: "Ratonero Bodeguero Andaluz",
+        image: "/images/bodi.jpg"
+    },
+    {
+        id: 3,
+        title: "American Staffordshire Terrier",
+        image: "/images/amstaff_head.jpg"
+    },
+    {
+        id: 4,
+        title: "Beagle",
+        image: "/images/beagle.jpg"
+    },
+    {
+        id: 5,
+        title: "Golden Retriever",
+        image: "/images/goldenRetriever.jpg"
+    },
+    {
+        id: 6,
+        title: "Australian Cattle Dog",
+        image: "/images/australianCattleDog_head.jpg"
+    },
+    {
+        id: 7,
+        title: "St. Bernard",
+        image: "/images/stBerard_front.jpg"
+    },
+    {
+        id: 8,
+        title: "Pyrenees Mountain Dog",
+        image: "/images/pyreneesMountainDog.jpg"
+    },
+    {
+        id: 9,
+        title: "Malinois",
+        image: "/images/malinois.jpg"
+    },
+    {
+        id: 10,
+        title: "Dogo Argentino",
+        image: "/images/dogoArgentino.jpg"
+    }
+];
+
